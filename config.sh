75b1fe7db511a449163f6f8649a8d8f8d763addf
Lucas-Wye <lustrew@foxmail.com>
2021-04-16 15:11:42 +0800

    My Rime Configuration

--- config.sh
+++ config.sh
@@ -2,4 +2,8 @@ for file in `ls sys/`; do
 	mv ~/.$file sys/$file.bak
 	mv sys/$file ~/.$file
 	mv sys/$file.bak sys/$file
-done	
+done
+
+# Rime Config For Linux
+cp rime/double_pinyin_flypy.* ~/.config/ibus/rime
+cp rime/ibus_rime.yaml rime/default.* ~/.config/ibus/rime
---
+++ rime/README.md
@@ -0,0 +1,2 @@
+sudo apt install -y ibus-rime librime-data-double-pinyin
+

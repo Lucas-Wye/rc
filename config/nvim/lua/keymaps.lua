7e92188e6772781360c9a660759ade08e24a112a
Lucas-Wye <lustrew@foxmail.com>
2024-04-15 09:45:52 +0800

    make the key of nvim the same as helix

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -17,7 +17,7 @@ vim.keymap.set('n', '<leader>k', '<C-w>k', opts)
 vim.keymap.set('n', '<leader>l', '<C-w>l', opts)
 
 -- 关闭所有buffer
-vim.keymap.set('n', '<leader>bd', ':bufdo bd<CR>', opts)
+-- vim.keymap.set('n', '<leader>bd', ':bufdo bd<CR>', opts)
 
 -- disable 's'
 vim.keymap.set('n', 's', '<nop>', opts)
@@ -32,23 +32,28 @@ vim.keymap.set('n', 'sr', ':set splitright<CR>:vsplit<CR>',                     
 vim.keymap.set('n', '<leader>n', ':lua print(vim.inspect(vim.fn.searchcount { maxcount = 0 }))<CR>', opts)
 
 -- resize
-vim.keymap.set('n', '<up>',    ':res +5<CR>',            opts)
-vim.keymap.set('n', '<down>',  ':res -5<CR>',            opts)
-vim.keymap.set('n', '<left>',  ':vertical resize-5<CR>', opts)
-vim.keymap.set('n', '<right>', ':vertical resize+5<CR>', opts)
+-- vim.keymap.set('n', '<up>',    ':res +5<CR>',            opts)
+-- vim.keymap.set('n', '<down>',  ':res -5<CR>',            opts)
+-- vim.keymap.set('n', '<left>',  ':vertical resize-5<CR>', opts)
+-- vim.keymap.set('n', '<right>', ':vertical resize+5<CR>', opts)
 
 -- rotate windows
-vim.keymap.set('n', 'srh', '<C-w>b<C-w>K', opts)
-vim.keymap.set('n', 'srv', '<C-w>b<C-w>H', opts)
+-- vim.keymap.set('n', 'srh', '<C-w>b<C-w>K', opts)
+-- vim.keymap.set('n', 'srv', '<C-w>b<C-w>H', opts)
 
 -- tab
-vim.keymap.set('n', 'ts',      ':tab split<CR>',        opts)
+-- vim.keymap.set('n', 'ts', ':tab split<CR>', opts)
 -- `gt` and `gT` to go through each tab
 
 -- quickfix
 vim.keymap.set('n', '<leader>cn', ':cn<CR>', opts)
 vim.keymap.set('n', '<leader>cp', ':cp<CR>', opts)
 vim.keymap.set('n', '<leader>co', ':copen<CR>', opts)
+
+-- goto
+vim.keymap.set('n', 'ge', 'G', opts)
+vim.keymap.set('n', 'gh', '0', opts)
+vim.keymap.set('n', 'gl', '$', opts)
 
 -----------------
 -- Visual mode --
@@ -58,5 +63,6 @@ vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 -- 搜索选中的内容
 vim.keymap.set('v', '@', 'y/<c-r>"<cr>', opts)
+
 -- 自动选中单词并搜索
-vim.keymap.set('v', '//', 'ey/\\<<c-r>"\\><cr>', opts)
+-- use `*`
